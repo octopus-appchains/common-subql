6e79984dcf0b96df91df679b92f5aa8e31558cd9
chezhe <chezhe@hey.com>
2023-12-04 13:56:28 +0800

    fix myriad extrinsic undefined

--- src/mappings/handleTransfer.ts
+++ src/mappings/handleTransfer.ts
@@ -20,26 +20,28 @@ async function checkAccount(accountId: string, timestamp: Date) {
 
 export async function handleTransfer(_event: SubstrateEvent): Promise<void> {
   const { event: evt, block, extrinsic } = _event
-  const {
-    data: [from_origin, to_origin, amount_origin],
-  } = evt
-  const from = (from_origin as AccountId).toString()
-  const to = (to_origin as AccountId).toString()
-  const amount = (amount_origin as Balance).toBigInt()
+  if (extrinsic) {
+    const {
+      data: [from_origin, to_origin, amount_origin],
+    } = evt
+    const from = (from_origin as AccountId).toString()
+    const to = (to_origin as AccountId).toString()
+    const amount = (amount_origin as Balance).toBigInt()
 
-  const record = new SystemTokenTransfer(
-    `${block.block.header.number.toString()}-${extrinsic.idx}`
-  )
-  record.fromId = from
-  record.toId = to
-  record.amount = amount
-  record.timestamp = block.timestamp
-  record.extrinsicId = extrinsic.extrinsic.hash.toString()
-  await Promise.all([
-    checkAccount(from, block.timestamp),
-    checkAccount(to, block.timestamp),
-    handleExtrinsic(extrinsic, block),
-    record.save(),
-    // handleEvent(block, _event, extrinsic),
-  ])
+    const record = new SystemTokenTransfer(
+      `${block.block.header.number.toString()}-${extrinsic.idx}`
+    )
+    record.fromId = from
+    record.toId = to
+    record.amount = amount
+    record.timestamp = block.timestamp
+    record.extrinsicId = extrinsic.extrinsic.hash.toString()
+    await Promise.all([
+      checkAccount(from, block.timestamp),
+      checkAccount(to, block.timestamp),
+      handleExtrinsic(extrinsic, block),
+      record.save(),
+      // handleEvent(block, _event, extrinsic),
+    ])
+  }
 }
